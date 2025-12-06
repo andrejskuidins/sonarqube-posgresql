7000c5829aa3c5b350aa5c5b37d3d95eb6213949
Andrejs.Kuidins <andrejs.kuidins@1nce.com>
2025-12-06 21:58:27 +0200

    feature/JIRA-02-postres-sonar

--- launch.sh
+++ launch.sh
@@ -1,28 +1,51 @@
-go install sigs.k8s.io/kind@v0.24.0
+#!/bin/bash
+set -e  # Exit on any error
+
+
+go install sigs.k8s.io/kind@v0.30.0
 export PATH=$HOME/.local/bin:$HOME/go/bin:$PATH
-cat <<EOF | kind create cluster --config=-
+
+
+# --- CHECK FOR EXISTING CLUSTER ---
+if kind get clusters | grep -q "^kind$"; then
+  echo "Cluster 'kind' already exists. Skipping creation."
+else
+  echo "Creating cluster 'kind'..."
+  cat <<EOF | kind create cluster --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
 - role: control-plane
-  kubeadmConfigPatches:
-  - |
-    kind: InitConfiguration
-    nodeRegistration:
-      kubeletExtraArgs:
-        node-labels: "ingress-ready=true"
   extraPortMappings:
-  - containerPort: 30000
-    hostPort: 30000
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+  - containerPort: 9000
+    hostPort: 9000
+    protocol: TCP
+  - containerPort: 5432
+    hostPort: 5432
     protocol: TCP
 EOF
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-helm install postgres -f pg.yml oci://registry-1.docker.io/bitnamicharts/postgresql
-sleep 90
-kubectl apply -f secret.yml
-helm repo add oteemocharts https://oteemo.github.io/charts
-helm install sonarqube -f sq.yml oteemocharts/sonarqube
+fi
 
-# Sounarsource option as oteemo is deprecated
-# helm repo add sonarqube https://SonarSource.github.io/helm-chart-sonarqube
-# helm upgrade --install sonarqube -f sonarqube.yml sonarqube/sonarqube
+
+echo "Waiting for cluster to be ready..."
+kubectl wait --for=condition=Ready nodes --all --timeout=300s
+kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml
+echo "Cluster created successfully!"
+echo ###############################
+
+kubectl apply -f secret.yml
+helm upgrade --install postgresql -f postgres.yml oci://registry-1.docker.io/bitnamicharts/postgresql
+echo "Waiting for PostgreSQL to be ready..."
+kubectl wait --for=condition=Ready pod/postgresql-0 \
+  --namespace=default --timeout=300s
+helm repo add sonarqube https://SonarSource.github.io/helm-chart-sonarqube
+helm repo update
+helm upgrade --install sonarqube -f sonarqube.yml  sonarqube/sonarqube
+
+kubectl apply -f pgadmin.yml
